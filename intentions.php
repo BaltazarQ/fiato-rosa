897c644e9686ea5ef0218efdad014bae11247373
BaltazarQ <prokopstanislav@gmail.com>
2022-09-09 11:07:15 +0200

    Uprava html struktuy v 'intentions.php'

--- intentions.php
+++ intentions.php
@@ -1,5 +1,3 @@
-<aside class="intentions">
-
 <?php
 
     // ZAPISANIE UMYSLU DO DATABAZY
@@ -16,45 +14,39 @@
             
             if(!$result){
                 die('Odoslanie do databazy zlyhalo'.mysqli_error());
-             } else {           
-                echo 'Chýba meno, heslo alebo potvrdenie hesla.';
-            }
-             
-            //  echo 'umysel zapisany do databazy';
-            //  header('Location: members.php');
+             }
         }
     }
 ?>
 
+<aside class="intentions">
     <section id="">
         <h3>Naše úmysly</h3>
         <div class="int-list">
 
-    <?php
+        <?php
 
-// VYPÍSANIE ÚMYSLOV Z DATABÁZY
-        $connection = mysqli_connect('localhost', 'root', '', 'fiato_login');
-        $query = "SELECT * FROM intentions";
+            // VYPÍSANIE ÚMYSLOV Z DATABÁZY
+            $connection = mysqli_connect('localhost', 'root', '', 'fiato_login');
+            $query = "SELECT * FROM intentions";
 
-        $result = mysqli_query($connection, $query);
+            $result = mysqli_query($connection, $query);
 
-        while($row = mysqli_fetch_assoc($result)){
-            $id = $row['id'];
-            $intention = $row['intention'];
-            echo('<div class="result-list">
-                    <a name="oneIntention" href="edit.php?id=' . $id . '&intention=' . $intention .'" class="one-intention">' . $row["intention"] . '</a>
-                </div>');
-        }
-
-    ?>
-
+            while($row = mysqli_fetch_assoc($result)){
+                $id = $row['id'];
+                $intention = $row['intention'];
+                echo('<div class="result-list">
+                        <a name="oneIntention" href="edit.php?id=' . $id . '&intention=' . $intention .'" class="one-intention">' . $row["intention"] . '</a>
+                    </div>');
+            }
+        ?>
 
         </div>
+        
         <form class="intentions-form" method="POST">
             <input type="text" name="intention" id="intentions-text" placeholder="Pridaj svoj úmysel">
             <input type="submit" name="intentionSubmit" value="Pridaj">
         </form>
-        <p class="notes">Kliknutím na úmysel ho môžeš zmeniť.</p>
-        <p class="notes">Kliknutím na krížik ho môžeš vymazať.</p>
+        <p class="notes">Kliknutím na úmysel ho môžeš zmeniť alebo vymazať.</p>
     </section>
 </aside>
