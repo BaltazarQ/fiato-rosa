876307d68f30279682e52111f7caa710a7dc419c
BaltazarQ <prokopstanislav@gmail.com>
2022-09-12 13:37:30 +0200

    Uprava kontroly zadanych nespravnych udajov do formulara login a register na 'login.php'

--- login.php
+++ login.php
@@ -1,3 +1,92 @@
+<?php
+        // REGISTRATION FORM
+        function registrationForm() {
+            if(isset($_POST['registrationSubmit'])){
+                $username = $_POST['username'];
+                $password = $_POST['password'];
+                $controlPassword = $_POST['controlPassword'];
+        
+                // zahashovanie hesla
+                $hashFormat = "$2y$10$";
+                $salt = "u9YPT1kh13fEPGlMmkWrID";
+                $hashFormat_salt = $hashFormat.$salt;
+                
+                $password = crypt($password, $hashFormat_salt);
+                $controlPassword = crypt($controlPassword, $hashFormat_salt);
+                // pripojenie do databazy
+                $connection = mysqli_connect('localhost', 'root', '', 'fiato_login');
+                $password = strval($password);
+
+        
+                // if($connection){
+                //     echo 'sme pripojeni k databaze';
+                // } else {
+                //     echo 'nepripojeni k databaze, niekde je chyba';
+                // }
+        
+                if ($username === ''){
+                    echo 'Chýba meno.';
+                } else if (empty($_POST['password'])) {
+                    echo 'Chýba heslo.';
+                } else if (empty($_POST['password']) && empty($_POST['controlPassword'])) {
+                    echo 'Chýba heslo a potvrdenie hesla.';
+                } else if (empty($_POST['controlPassword'])) {
+                    echo 'Chýba potvrdenie hesla.';
+                } else if ($controlPassword !== $password) {
+                    echo 'Heslá sa nezhodujú.';
+                } else {
+                    // odoslanie dat do databazy
+                    $query = "INSERT INTO users(username, password) VALUES('$username', '$password')";
+                    $result = mysqli_query($connection, $query);
+                    
+                    if(!$result){
+                        die('Odoslanie do databazy zlyhalo'.mysqli_error());
+                    }
+                    
+                    header('Location: members.php');
+                }
+            }
+        }
+    
+
+    // LOGIN FORM
+function loginForm() {
+
+    if(isset($_POST['loginSubmit'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        // zahashovanie hesla
+        $hashFormat = "$2y$10$";
+        $salt = "u9YPT1kh13fEPGlMmkWrID";
+        $hashFormat_salt = $hashFormat.$salt;
+        
+        $password = crypt($password, $hashFormat_salt);
+
+        // pripojenie do databazy
+        $connection = mysqli_connect('localhost', 'root', '', 'fiato_login');
+
+        // vyber dat z databazy podla prihlasovacieho mena
+        $controlPassword = "SELECT * FROM users";
+
+        $controlResult = mysqli_query($connection, $controlPassword);
+        $controlRow = mysqli_fetch_assoc($controlResult);
+
+        // ak bude meno a heslo totozne s udajmi z databazy, presmeruj ma na stranku members.php
+        if ($username === '') {
+            echo 'Chýba meno';
+        } else if ($password === '' || $controlRow["password"] !== $password) {
+            echo 'Zlé heslo';
+        } else if($controlRow["username"] !== $username || $controlRow["password"] !== $password) {
+            echo 'Zlé meno alebo heslo';
+        } else if($controlRow["username"] === $username && $controlRow["password"] === $password) {
+            // echo 'uspesne prihlaseny';
+            header('Location: members.php');
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,106 +111,25 @@
             <h3><a class="reg-choice" href="#">Registrácia</a></h3>
             <h3><a class="login-choice" href="#">Prihlásenie</a></h3>
         </div>
-        <form id="registration-form" class="my-form" method="POST">
+        <form id="registration-form" class="my-form" method="POST" action="login.php">
             <input type="text" name="username" class="login-name" placeholder="Zadaj meno">
             <input type="password" name="password" class="login-password" placeholder="Zadaj heslo">
             <input type="password" name="controlPassword" class="login-password" placeholder="Zopakuj heslo">
             <input type="submit" name="registrationSubmit" value="Potvď" id="submit">
         </form>
+        <div class="wrong-data">
+            <p class="wrong-data-text"><?php registrationForm()?></p>
+        </div>
 
-<?php
-        // REGISTRATION FORM
-    if(isset($_POST['registrationSubmit'])){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-        $controlPassword = $_POST['controlPassword'];
-        $succesRegistration = 'Boli ste úspešne prihlásený.';
-        $failurRegistration = 'Prihlásenie neúspešné, skontrolujte meno a/alebo heslo.';
-
-        // zahashovanie hesla
-        $hashFormat = "$2y$10$";
-        $salt = "u9YPT1kh13fEPGlMmkWrID";
-        $hashFormat_salt = $hashFormat.$salt;
-        
-        $password = crypt($password, $hashFormat_salt);
-        $controlPassword = crypt($controlPassword, $hashFormat_salt);
-        // pripojenie do databazy
-        $connection = mysqli_connect('localhost', 'root', '', 'fiato_login');
-
-        // if($connection){
-        //     echo 'sme pripojeni k databaze';
-        // } else {
-        //     echo 'nepripojeni k databaze, niekde je chyba';
-        // }
-
-    if ($username === ''){
-           echo 'Chýba meno.';
-    } else if ($password === '') {
-        echo 'Chýba heslo.';
-    } else if ($controlPassword === '') {
-        echo 'Chýba potvrdenie hesla.';
-    } else if ($controlPassword !== $password) {
-        echo 'Heslá sa nezhodujú.';
-    } else {
-
-        // odoslanie dat do databazy
-        $query = "INSERT INTO users(username, password) VALUES('$username', '$password')";
-        $result = mysqli_query($connection, $query);
-        
-        if(!$result){
-            die('Odoslanie do databazy zlyhalo'.mysqli_error());
-         }
-         
-         header('Location: members.php');
-    }
-    }
-?>
-        <!-- <div class="wrong-data">
-            <p class="wrong-data-text"></p>
-        </div> -->
-
-        <form id="login-form" class="my-form" method="POST">
+        <form id="login-form" class="my-form" method="POST" action="login.php">
             <input type="text" name="username" class="login-name" placeholder="Zadaj meno">
             <input type="password" name="password" class="login-password" placeholder="Zadaj heslo">
             <input type="submit" name="loginSubmit" value="Potvď" id="submit">
         </form>
 
-<?php
-
-    // LOGIN FORM
-    if(isset($_POST['loginSubmit'])){
-        $username = $_POST['username'];
-        $password = $_POST['password'];
-
-        // zahashovanie hesla
-        $hashFormat = "$2y$10$";
-        $salt = "u9YPT1kh13fEPGlMmkWrID";
-        $hashFormat_salt = $hashFormat.$salt;
-        
-        $password = crypt($password, $hashFormat_salt);
-
-        // pripojenie do databazy
-        $connection = mysqli_connect('localhost', 'root', '', 'fiato_login');
-
-        // vyber dat z databazy podla prihlasovacieho mena
-        $controlPassword = "SELECT * FROM users WHERE username='$username'";
-
-        $controlResult = mysqli_query($connection, $controlPassword);
-        $controlRow = mysqli_fetch_assoc($controlResult);
-
-        // ak bude meno a heslo totozne s udajmi z databazy, presmeruj ma na stranku members.php
-        if($username === $controlRow["username"] && $password === $controlRow["password"]) {
-            echo 'uspesne prihlaseny';
-            header('Location: members.php');
-        } else {
-            echo 'zle meno alebo heslo';
-        }
-    }
-?>
-
-        <!-- <div class="wrong-data">
-            <p class="wrong-data-text"></p>
-        </div> -->
+        <div class="wrong-data">
+            <p class="wrong-data-text"><?php loginForm()?></p>
+        </div>
 
     </main>
     
